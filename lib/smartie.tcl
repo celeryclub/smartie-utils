1c8f4b05068c46587acf357c17fc604133410ab9
Alexander Galanin <al@galanin.nnov.ru>
2013-11-05 20:30:25 +0400

    added Cyrillic support

--- lib/smartie.tcl
+++ lib/smartie.tcl
@@ -33,7 +33,9 @@ namespace export \
     destroy \
     contrast \
     writeLine \
-    customChar
+    customChar \
+    loadCyrillicChars \
+    convertCyrillicText
 
 # Open serial port and set parameters
 # @param port   Device (for example, /dev/ttyUSB0)
@@ -123,6 +125,92 @@ proc writeLine {fd line str} {
 proc customChar {fd char d0 d1 d2 d3 d4 d5 d6 d7} {
     command $fd acc8 \
         N $char [list $d0 $d1 $d2 $d3 $d4 $d5 $d6 $d7]
+}
+
+# Define custom characters for displaying Cyrillic text
+proc loadCyrillicChars {fd} {
+    customChar $fd 0 0x12 0x12 0x12 0x12 0x12 0x12 0x1f 1
+    customChar $fd 1 0x15 0x15 0x15 0x15 0x15 0x15 0x1f 0
+    customChar $fd 2 0x11 0x11 0x11 0x19 0x15 0x15 0x19 0
+    customChar $fd 3 0x1f 0x11 0x11 0x11 0x11 0x11 0x11 0
+    customChar $fd 4 0x15 0x15 0x15 0xe 0x15 0x15 0x15 0
+    customChar $fd 5 0xf 0x11 0x11 0xf 0x11 0x11 0x11 0
+    customChar $fd 6 0x1f 0x10 0x10 0x1e 0x11 0x11 0x1e 0
+    customChar $fd 7 0x12 0x15 0x15 0x1d 0x15 0x15 0x12 0
+}
+
+# Replace chars in Cyrillic text to be viewed using Smartie
+proc convertCyrillicText {text} {
+    string map {
+        \u0419   \xf5
+        \u0439   \xf5
+        \u0426   \x00
+        \u0446   \x00
+        \u0423   Y
+        \u0443   y
+        \u041A   K
+        \u043A   k
+        \u0415   E
+        \u0435   e
+        \u041D   H
+        \u043D   H
+        \u0413   \xa2
+        \u0433   \xa2
+        \u0428   \x01
+        \u0448   \x01
+        \u0429   \x01
+        \u0449   \x01
+        \u0417   3
+        \u0437   3
+        \u0425   X
+        \u0445   x
+        \u042A   b
+        \u044A   b
+        \u0424   \xec
+        \u0444   \xec
+        \u042B   \x02
+        \u044B   \x02
+        \u0412   B
+        \u0432   B
+        \u0410   A
+        \u0430   a
+        \u041F   \x03
+        \u043F   \x03
+        \u0420   P
+        \u0440   p
+        \u041E   O
+        \u043E   o
+        \u041B   \xca
+        \u043B   \xca
+        \u0414   D
+        \u0434   d
+        \u0416   \x04
+        \u0436   \x04
+        \u042D   \xd6
+        \u044D   \xae
+        \u042F   \x05
+        \u044F   \x05
+        \u0427   4
+        \u0447   4
+        \u0421   C
+        \u0441   c
+        \u041C   M
+        \u043C   M
+        \u0418   \xe4
+        \u0438   \xe4
+        \u0422   T
+        \u0442   T
+        \u042C   b
+        \u044C   b
+        \u0411   \x06
+        \u0431   \x06
+        \u042E   \x07
+        \u044E   \x07
+        \u0401   E
+        \u0451   e
+        \\  \xa4
+        ~   \xf3
+    } $text
 }
 
 }
